67691fec23188fb6ffce32dad96ac31e07a2ac84
Ugo-gonpi <ugogonzalez50@gmail.com>
2024-01-23 15:13:12 +0100

    did the function that adds tasks

--- public/js/main.js
+++ public/js/main.js
@@ -1,1 +1,27 @@
+let entrer = document.querySelector(".entrer")
 
+let trierTous = document.querySelector(".tous")
+
+let trierFinit = document.querySelector(".faite")
+
+let trierEnCours = document.querySelector(".enCours")
+
+let btnFinit = document.querySelector(".finit")
+
+let btnMod = document.querySelector(".modifier")
+
+let btnSupr = document.querySelector(".effacer")
+
+let body = document.body
+
+let tache = document.querySelector(".listeTaches")
+
+
+function rajoutTache() {
+    entrer.addEventListener("click", () => {
+        let clone = tache.cloneNode(true)
+        body.appendChild(clone)
+    })
+}
+
+rajoutTache()
